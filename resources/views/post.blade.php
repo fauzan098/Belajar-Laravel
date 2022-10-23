e4822d295b72104b364bac872d354f5d01c57273
fauzan098 <muhammadariffauzan03@gmail.com>
2022-10-23 18:06:30 +0700

    update

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,12 +2,20 @@
 
 @extends('layouts.main')
 @section('container')
-    <article>
-        <h1>{{ $post->title }}</h1>
 
-        <p>By. <a href="/authors/{{ $post->author->username }}"></a> in <a href="/categories/{{ $post->category->slug }}" class="me-2" >{{ $post->author->name }}</a>{{ $post->category->name }}</p>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h1 class="mb-3">{{ $post->title }}</h1>
+                <p>By <a href="/authors/{{ $post->author->username }}" class="me-2" >{{ $post->author->name }}</a>in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
-        {!! $post->body !!}
-    </article>
-    <a href="/posts">Back to Posts</a>
+                    <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+                    <article class="my-3 fs-5">
+                        {!! $post->body !!}
+                    </article>
+                <a href="/posts">Back to Posts</a>
+            </div>
+        </div>
+    </div>
+        
 @endsection
