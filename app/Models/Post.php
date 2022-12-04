e584197ccb9e0c14b430a8373d04eecf1e9f7ae2
fauzan098 <muhammadariffauzan03@gmail.com>
2022-12-04 15:12:18 +0700

    updated

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
     
     protected $guarded = ['id'];
     protected $with = ['category','author'];
@@ -42,8 +44,23 @@ class Post extends Model
         return $this-> belongsTo(Category::class);
     }
 
+    // menghubungkan post ke user.
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
